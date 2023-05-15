ba255ab0533ad37774bdec6e513824e63cdf5695
taytayAndrew <1273724496@qq.com>
2023-05-15 19:18:58 +0800

    Tabs 支出自定义三处样式

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,28 +1,49 @@
+import { ReactNode } from 'react';
 import s from './Tabs.module.scss'
+import styled from 'styled-components';
+import cs from 'classnames'
 type Props<T> = {
     tableItems:{
         key: T;
         text: string;
+        element?: ReactNode;
     }[]
     value: T
     onChange: (key: T) => void
     className?: string
+    classPrefix?: string
 }
+const Styledol = styled.ol`
+background: linear-gradient(90deg, rgba(9,121,101,1) 0%, rgba(204,187,231,1) 0%, rgba(107,247,255,1) 94%);
 
+`
 export const Tabs = <T extends string>(props:Props<T> ) => {
-    const {tableItems , value , onChange, className} = props
+    const {tableItems , value , onChange, className,classPrefix} = props
   return (
-    <ol className={className} flex flex-row children-px-24px children-py-16px cursor-pointer>
+    <div className={cs(className,classPrefix)}>
+    <Styledol>
+         <ol flex flex-row children-px-24px children-py-16px cursor-pointer className={classPrefix ? `${classPrefix}-menu`: ''} >
       {tableItems.map( item => (
         <li
           pointer-cursor
-          className={item.key === value ? s.selected : ""}
+          className={
+            cs(
+                item.key === value ? s.selected : "",
+                classPrefix ? `${classPrefix}-menu-item`: ''
+            )
+            }
           onClick={() => onChange(item.key)}
           key={item.key}
         >
           {item.text}
         </li>
       ))}
-    </ol>
-  );
+      </ol>
+    </Styledol>
+    <div className={classPrefix ? `${classPrefix}-panes`: ''}>
+           {tableItems.filter(item => item.key === value)[0].element}
+    </div>
+ 
+    </ div>
+  )
 };
--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react"
+import { ReactNode, useState } from "react"
 import { Gradient } from "../components/Gradient"
 import { Icon } from "../components/Icon"
 import { Tabs } from "../components/Tab"
 import { Topnav } from "../components/Topnav"
+import s from "./ItemsNewPage.module.scss"
 
 export const ItemsNewPage :React.FC = () =>{
-  const tabItems:{key: 'income' | 'expenses' ; text: string}[] = [{key: 'income', text: '收入' },{key: 'expenses', text: '支出' }];
+  const tabItems:{key: 'income' | 'expenses' ; text: string; element?:ReactNode}[] = [{key: 'income', text: '收入',element:<div>收入</div> },{key: 'expenses', text: '支出',element:<div>支出</div> }];
   const [tabItem , setTabItems] = useState<'income' | 'expenses'>('expenses')
  return (
     <>
     <Gradient>
       <Topnav title='记一笔' icon={<Icon name='back'/>}/>
-      <Tabs className='children-flex-1 text-center' tableItems={tabItems} value={tabItem} onChange={(item) =>{setTabItems(item)}} />
-    </Gradient>
+      </Gradient>
+      <Tabs className={s.wrapper} tableItems={tabItems} value={tabItem} onChange={(item) =>{setTabItems(item)}}  classPrefix='itemsNewTabs' />
+   
     </>)
   
 }
