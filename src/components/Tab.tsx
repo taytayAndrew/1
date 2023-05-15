a70f6b2c71676c6eac50944e241c3f4f0e1e40c4
taytayAndrew <1273724496@qq.com>
2023-05-15 18:03:46 +0800

    完成记账页面的导航目录

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -6,12 +6,13 @@ type Props<T> = {
     }[]
     value: T
     onChange: (key: T) => void
+    className?: string
 }
 
 export const Tabs = <T extends string>(props:Props<T> ) => {
-    const {tableItems , value , onChange} = props
+    const {tableItems , value , onChange, className} = props
   return (
-    <ol flex flex-row children-px-24px children-py-16px cursor-pointer>
+    <ol className={className} flex flex-row children-px-24px children-py-16px cursor-pointer>
       {tableItems.map( item => (
         <li
           pointer-cursor
--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import { Gradient } from "../components/Gradient"
 import { Icon } from "../components/Icon"
+import { Tabs } from "../components/Tab"
 import { Topnav } from "../components/Topnav"
 
 export const ItemsNewPage :React.FC = () =>{
+  const tabItems:{key: 'income' | 'expenses' ; text: string}[] = [{key: 'income', text: '收入' },{key: 'expenses', text: '支出' }];
+  const [tabItem , setTabItems] = useState<'income' | 'expenses'>('expenses')
  return (
     <>
     <Gradient>
       <Topnav title='记一笔' icon={<Icon name='back'/>}/>
+      <Tabs className='children-flex-1 text-center' tableItems={tabItems} value={tabItem} onChange={(item) =>{setTabItems(item)}} />
     </Gradient>
     </>)
   
