f92c5d62c6bd73eac68bcc94e6c8033950f3e5fa
taytayAndrew <1273724496@qq.com>
2023-05-17 18:04:01 +0800

    使用grid布局完成标签

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,20 +1,39 @@
-import { ReactNode, useState } from "react"
-import { Gradient } from "../components/Gradient"
-import { Icon } from "../components/Icon"
-import { Tabs } from "../components/Tab"
-import { Topnav } from "../components/Topnav"
-import s from "./ItemsNewPage.module.scss"
+import { ReactNode, useState } from "react";
+import { Gradient } from "../components/Gradient";
+import { Icon } from "../components/Icon";
+import { Tabs } from "../components/Tab";
+import { Topnav } from "../components/Topnav";
+import s from "./ItemsNewPage.module.scss";
+import { Tags } from "./ItemsNewPage/Tags";
 
-export const ItemsNewPage :React.FC = () =>{
-  const tabItems:{key: 'income' | 'expenses' ; text: string; element?:ReactNode}[] = [{key: 'income', text: '收入',element:<div>收入</div> },{key: 'expenses', text: '支出',element:<div>支出</div> }];
-  const [tabItem , setTabItems] = useState<'income' | 'expenses'>('expenses')
- return (
-    <>
-    <Gradient>
-      <Topnav title='记一笔' icon={<Icon name='back'/>}/>
+
+export const ItemsNewPage: React.FC = () => {
+  const tabItems: {
+    key: "income" | "expenses";
+    text: string;
+    element?: ReactNode;
+  }[] = [
+    { key: "income", text: "收入", element:
+    <Tags kind="expenses" />  },
+    { key: "expenses", text: "支出", element: 
+    <Tags kind="income" />
+  },
+  ];
+  const [tabItem, setTabItems] = useState<Item['kind']>("expenses");
+  return (
+    <div className={s.wrapper}>
+      <Gradient>
+        <Topnav title="记一笔" icon={<Icon name="back" />} />
       </Gradient>
-      <Tabs className={s.wrapper} tableItems={tabItems} value={tabItem} onChange={(item) =>{setTabItems(item)}}  classPrefix='itemsNewTabs' />
-   
-    </>)
-  
-}
+      <Tabs
+        className='text-center'
+        tableItems={tabItems}
+        value={tabItem}
+        onChange={(tabitem) => {
+          setTabItems(tabitem);
+        }}
+        classPrefix="itemsNewTabs"
+      />
+    </ div>
+  );
+};
---
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -0,0 +1,18 @@
+
+type Props= {
+  kind: Item['kind']
+}
+export const Tags: React.FC<Props> = (props) => {
+  const {kind} =  props
+  const tags = Array.from({ length: 99 });
+  return (
+    <div>
+      <ol grid grid-cols="[repeat(auto-fit,48px)]" justify-center gap-x-32px gap-y-16px py-16px px-16px>
+        {tags.map(tag =>
+        <li w-48px h-48px b-1 b-red flex justify-center items-center>🥵</li>)
+
+        }
+      </ol>
+    </div>
+  );
+};
