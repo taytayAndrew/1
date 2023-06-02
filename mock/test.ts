f9c53a47458f17aaf6aa4fcf90300077a74493b7
taytayAndrew <1273724496@qq.com>
2023-06-02 15:41:03 +0800

    获取tags第一页数据

--- mock/test.ts
+++ mock/test.ts
@@ -2,9 +2,10 @@ import type { MockMethod } from 'vite-plugin-mock'
 import { meMock } from './me.mock'
 import { itemsMock } from './items.mock'
 import { sessionMock } from './sessionmock'
+import { tagsMock } from './tag.mocks'
 export default [
   itemsMock,
   meMock,
-  sessionMock
- 
+  sessionMock,
+  tagsMock
 ] as MockMethod[]
--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { Icon } from "../../components/Icon";
+import { useTagsStore } from "../../stores/useTagsStore";
+import { useAjax } from "../../lib/ajax";
+import useSwr from 'swr'
 
 type Props= {
   kind: Item['kind']
@@ -8,16 +11,12 @@ type Props= {
 }
 export const Tags: React.FC<Props> = (props) => {
   const {kind,value,onChange} =  props
-  const tags = Array.from({ length: 99 }).map<Tag>((tag,index) =>({
-    id: index,
-    name: `打车${index}`,
-    kind: 'expenses',
-    sign: '🥵',
-    user_id: 1,
-    created_at: '2000-01-01T00:00:00.000Z',
-    updated_at: '2000-01-01T00:00:00.000Z',
-    deleted_at: null
-  }));
+  const {list : tags, setList}= useTagsStore()
+  const {get} = useAjax({showLoading: true , handleError:true})
+  useSwr('/api/v1/tags' , async (path) => {
+    const response = await get<Resources<Tag>>(path)
+    setList(response.data.resources)
+  })
   return (
     <div>
       <ol grid grid-cols="[repeat(auto-fit,48px)]" justify-center gap-x-32px gap-y-16px py-16px px-16px>
