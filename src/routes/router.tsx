29929f282e383ed0c2eb2bea53c2c0c84283c334
taytayAndrew <1273724496@qq.com>
2023-07-11 16:24:45 +0800

    修改deploy

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { Outlet, createBrowserRouter } from 'react-router-dom'
+import { Outlet, createBrowserRouter, createHashRouter } from 'react-router-dom'
 import type { AxiosError } from 'axios'
 import { Root } from '../components/Root'
 import { WelcomeLayout } from '../layouts/WelcomeLayout'
@@ -18,7 +18,7 @@ import { ItemPageError } from '../pages/ItemPageError'
 import { ajax } from '../lib/ajax'
 import { ComingSoonPage } from '../pages/ComingSoonPage'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: '/', element: <Root />, },
   { path: '/home', element: <Home title="首页" /> },
   {
