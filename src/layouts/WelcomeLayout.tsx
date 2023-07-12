28bf3139076808ee723c6197f85a7b35d8020c6c
taytayAndrew <1273724496@qq.com>
2023-07-12 15:18:40 +0800

    正式ok

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -65,8 +65,8 @@ export const WelcomeLayout: React.FC = () => {
     <div className="bg-#33cbda" h-screen flex flex-col items-stretch pb-16px>
       <span fixed text-white top-16px right-16px text-32px onClick={onSkip} >跳过</span>
       <header shrink-0 text-center pt-64px>
-        <img src={logo} w-64px h-69px />
-        <h1 text="#D4D4EE" text-32px>猫尾记账</h1>
+        <img w-100px h-100px bg-white rounded-50px src={logo} w-64px h-69px />
+        <h1 text="white"  text-32px >猫尾记账</h1>
       </header>
       <main shrink-1 grow-1 relative ref={main} >
         {transitions((style, pathname) =>
--- src/pages/ComingSoonPage.tsx
+++ src/pages/ComingSoonPage.tsx
@@ -9,7 +9,7 @@ export const ComingSoonPage:React.FC = () =>{
  return (
     <div flex flex-col justify-center items-center gap-y-24px h-screen px-48px>
 
-        <Icon name ='kitty' className='w-128px h-128px'/>
+        <Icon name ='comingsoon' className='w-128px h-128px'/>
         <div>敬请期待</div>
             <button onClick={() => nav(-1)} j-btn>返回</button>
 
